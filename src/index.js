09120b23c218f68378961e52ba7712a818348a0a
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-12-18 22:02:11 -0800

    WIP: initial integration attempt

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import xs from "xstream";
+import delay from "xstream/extra/delay";
 import React from "react";
 import ReactDOM from "react-dom";
 import { selectionDriver } from "cycle-selection-driver";
 import { makeComponent } from "@cycle/react";
-import { div, p, button, br } from "@cycle/react-dom";
+import { div, p, button, br, source } from "@cycle/react-dom";
+
+import { initializeDrivers, withActions } from "@cycle-robot-drivers/run";
 
 function highlighter(sources) {
   const left = Symbol();
@@ -61,10 +64,40 @@ const Highlighter = makeComponent(highlighter, {
   selection: selectionDriver
 });
 
+function robotMain(sources) {
+  sources.PoseDetection.poses
+    .addListener({next: () => {}});  // see outputs on the browser
+
+  sources.DOM.element()
+    .addListener({next: d => console.log("sources.PoseDetection.DOM", d)});  // see outputs on the browser
+
+  return {
+    // PoseDetection: xs.of({
+    //   algorithm: 'single-pose',
+    //   singlePoseDetection: {minPoseConfidence: 0.2},
+    // }),
+    // react: xs.combine(
+    //   sources.TwoSpeechbubblesAction.DOM,
+    //   sources.TabletFace.DOM,
+    //   sources.PoseDetection.DOM
+    // ).map(([speechbubbles, face, poseDetectionViz]) => {
+    //   poseDetectionViz.data.style.display = false
+    //     ? 'none' : 'block';
+    //   return div({
+    //     style: {position: 'relative'}
+    //   }, [speechbubbles, face, poseDetectionViz]);
+    // }),
+    react: sources.TabletFace.DOM.mapTo(div('Hello')).debug(),
+  };
+}
+
+const Robot = makeComponent(withActions(robotMain), initializeDrivers());
+
 function App(props) {
   return (
     <div className="app">
       <Highlighter text={"The book is on the table."} />
+      <Robot />
     </div>
   );
 }
